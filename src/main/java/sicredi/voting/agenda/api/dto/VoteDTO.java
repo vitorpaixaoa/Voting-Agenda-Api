1230fe99fb9b5f7239b59e5b8c495837aebc2c87
Alan Vitor Paixão <alan.brivia@unicesumar.edu.br>
2022-05-05 12:34:04 -0300

    :sparkles: Feature -> Adding Unit Tests

--- src/main/java/sicredi/voting/agenda/api/dto/VoteDTO.java
+++ src/main/java/sicredi/voting/agenda/api/dto/VoteDTO.java
@@ -1,13 +1,17 @@
 package sicredi.voting.agenda.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.br.CPF;
 
 import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class VoteDTO {
 
     @NotNull
